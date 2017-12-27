051b71210f2b44610ac5f2cae7aaa24fbea248f1
Robert Eng <robert.km.eng@gmail.com>
2017-12-27 15:17:55 -0800

    Change it back

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from WarStateMachine import War
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
-async_mode = 'gevent'
+async_mode = None
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
